b1ab5bca4287e64ab846029fbda5be2d4e701ede
Ruan Bekker <ruan.ru.bekker@gmail.com>
2019-03-31 16:37:25 +0200

    change wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,16 @@
-import sys
-sys.path.insert(0,"/var/www/webroot/ROOT/")
-print(environ)
-from myapp import app as application
+def application(environ, start_response):
+    import sys
+    path = '/var/www/webroot/ROOT'
+    if path not in sys.path:
+        sys.path.append(path)
+
+    #from pyinfo import pyinfo
+    #output = pyinfo()
+    output = environ
+    start_response('200 OK', [('Content-type', 'text/html')])
+    yield output.encode('utf-8')
+
+#import sys
+#sys.path.insert(0,"/var/www/webroot/ROOT/")
+#print(environ)
+#from myapp import app as application

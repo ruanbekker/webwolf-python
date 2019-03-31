ec02c356ba6dc5547acf836e85274ca66a306a80
Ruan Bekker <ruan.ru.bekker@gmail.com>
2019-03-31 16:10:34 +0200

    return var2

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -8,9 +8,13 @@ def hello():
     payload = {"status": 200, "message": "test message", "language": "python", "tags": ["programming", "web", "python"]}
     return jsonify(payload)
 
+@app.route("/hello")
+def hello1():
+    return "hello"
+
 @app.route("/envs")
 def envs():
-    return jsonify(os.environ)
+    return os.environ
 
 if __name__ == "__main__":
     app.run()

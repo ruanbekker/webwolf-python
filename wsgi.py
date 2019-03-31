8541def60fd227db1eec6ba6c17f5d2b2a68fd2d
Ruan Bekker <ruan.ru.bekker@gmail.com>
2019-03-31 17:00:56 +0200

    change wsgi 2

--- wsgi.py
+++ wsgi.py
@@ -1,20 +1,4 @@
-def application(environ, start_response):
-    import sys
-    path = '/var/www/webroot/ROOT'
-    if path not in sys.path:
-        sys.path.append(path)
-
-    #from pyinfo import pyinfo
-    #output = pyinfo()
-    output = environ['UNIQUE_ID']
-    start_response('200 OK', [('Content-type', 'text/html')])
-    #return str(output)
-    from myapp import app as application
-    #output = foo
-    #return output
-    #yield output.encode('utf-8')
-
-#import sys
-#sys.path.insert(0,"/var/www/webroot/ROOT/")
-#print(environ)
-#from myapp import app as application
+import sys
+sys.path.insert(0,"/var/www/webroot/ROOT/")
+environ
+from myapp import app as application

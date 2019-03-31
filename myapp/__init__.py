d796158f583f359926b34de3df7f19bb0ef54d3c
Ruan Bekker <ruan.ru.bekker@gmail.com>
2019-03-31 16:03:27 +0200

    return var2

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -1,13 +1,16 @@
 import os
-from flask import Flask
-
-name = os.environ['NAME']
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, {}".format(name)
+    payload = {"status": 200, "message": "test message", "language": "python", "tags": ["programming", "web", "python"]}
+    return jsonify(payload)
+
+@app.route("/envs")
+def envs():
+    return jsonify(os.environ)
 
 if __name__ == "__main__":
     app.run()

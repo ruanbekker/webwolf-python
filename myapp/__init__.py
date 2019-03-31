0fbe96b1ed2096528b5de6d7cbf36cb0e5cee7f4
Ruan Bekker <ruan.ru.bekker@gmail.com>
2019-03-31 15:54:03 +0200

    return var

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -1,10 +1,13 @@
+import os
 from flask import Flask
+
+name = os.environ['NAME']
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, Flask!"
+    return "Hello, {}".format(name)
 
 if __name__ == "__main__":
     app.run()
